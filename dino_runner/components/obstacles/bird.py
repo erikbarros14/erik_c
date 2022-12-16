089f28b1a6568d7674dea88596b1940a57ff7a37
Erik <erikbarros1402@gmail.com>
2022-12-16 19:38:18 -0300

    aula 5

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,17 +1,18 @@
 import random
 
-from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.components.obstacles.obstacles import Obstacle # importa obstacle
 
-class Bird(Obstacle):
+listay = [250, 320] # lista com posicaos aleatorias de y
 
-    def __init__(self,image):
-
-        self.type = 0
+class Bird (Obstacle): # cria bird recebendo obstacle
+    def __init__ (self, image): 
+        self.type = 0 # type é 0
         super().__init__(image, self.type)
-        self.rect.y = random.randint(250 ,280)
+        self.rect.y = random.choice(listay) # random para posição y dele ser aleatória
         self.index = 0
-    def draw(self, screen):
-        if self.index >= 9 :
-            self.index = 0
-        screen.blit (self.image[self.index//5], self.rect)
-        self.index += 1
+
+    def draw (self, screen): # metodo de draw
+        if self.index >= 9: # se o index for maior ou igual a 9
+            self.index = 0 # reseta
+        screen.blit (self.image[self.index//5], self.rect) # de 0 a 4 uma imagem, de 5 a 9 outra imagem
+        self.index += 1 # adiciona +1 no index a cada iteração
--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,15 +1,15 @@
 import random
 
-from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.components.obstacles.obstacles import Obstacle
 
-class Cactus(Obstacle):
-    def __init__(self, image):
+class Cactus (Obstacle):
+    def __init__ (self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 325
 
-class large_cactus(Obstacle):
-    def __init__(self, image):
-        self.type = random.randint(0,2)
+class Large_Cactus (Obstacle):
+    def __init__ (self, image):
+        self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 300
